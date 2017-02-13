8447bbb008a4af584a9d05167c220cdb8e88b1ea
Longster <longster@outlook.com>
2017-02-13 13:00:43 -0600

    layout update

--- lduong/src/assets/js/app.js
+++ lduong/src/assets/js/app.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
 
   $(document).foundation();
 	// grab the initial top offset of the navigation
-   	var stickyNavTop = $('#navbar').offset().top;
+ 	var stickyNavTop = $('#navbar').offset().top;
 
-   	// our function that decides weather the navigation bar should have "fixed" css position or not.
-   	var stickyNav = function(){
-	    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+ 	// function that decides wether the navigation bar should have "fixed" css position or not.
+ 	var stickyNav = function(){
+    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 
-	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-	    // otherwise change it back to relative
-	    if (scrollTop > stickyNavTop) {
-	        $('#navbar').addClass('sticky');
-          $('.sticksters').addClass('sticky-hack');
-	    } else {
-	        $('#navbar').removeClass('sticky');
-          $('.sticksters').removeClass('sticky-hack');
-	    }
+    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+    // otherwise change it back to relative
+    if (scrollTop > stickyNavTop) {
+        $('#navbar').addClass('sticky');
+        $('.sticksters').addClass('sticky-hack');
+    } else {
+        $('#navbar').removeClass('sticky');
+        $('.sticksters').removeClass('sticky-hack');
+    }
 	};
 
 	stickyNav();
@@ -25,5 +25,13 @@ $(document).ready(function() {
 		stickyNav();
 	});
 
+
+  // Anchor smooth scroll
+  $('a.scroll').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
 
 });
