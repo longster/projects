67522037acf4d581c23e54c15673de5d7a7c45ca
Longster <longster@live.com>
2017-02-18 21:33:45 -0600

    bug fixes and update projects gallery

--- lduong/src/assets/js/app.js
+++ lduong/src/assets/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
  	var stickyNavTop = $('#navbar').offset().top;
 
  	// function that decides wether the navigation bar should have "fixed" css position or not.
- 	var stickyNav = function(){
+ 	var stickyNav = function()  {
     var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 
     // if we've scrolled more than the navigation, change its position to fixed to stick to top,
@@ -24,6 +24,15 @@ $(document).ready(function() {
 	$(window).scroll(function() {
 		stickyNav();
 	});
+
+  // collapse responsive-menu after clicking link when on responsive mode - http://stackoverflow.com/questions/18516696/how-can-i-close-the-foundation-topbar-menu-when-a-link-is-clicked
+  $('#responsive-menu li a, .top-bar-title a').click(function() {
+    var width = Foundation.MediaQuery.current;
+
+    if( width == 'small' || width == 'medium'){
+        $('#responsive-menu').css('display', 'none');
+    }
+  });
 
 
   // Anchor smooth scroll
