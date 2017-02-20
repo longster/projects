31d85b8b052eb4ab8fe62600bd8069a6b50211a9
Longster <longster@outlook.com>
2017-02-20 07:37:29 -0600

    fix nav button bugs

--- lduong/src/assets/js/app.js
+++ lduong/src/assets/js/app.js
@@ -25,16 +25,24 @@ $(document).ready(function() {
 		stickyNav();
 	});
 
-  // collapse responsive-menu after clicking link when on responsive mode - http://stackoverflow.com/questions/18516696/how-can-i-close-the-foundation-topbar-menu-when-a-link-is-clicked
+  // Collapse responsive-menu after clicking link when on responsive mode:
+  // http://stackoverflow.com/questions/18516696/how-can-i-close-the-foundation-topbar-menu-when-a-link-is-clicked
+  // http://foundation.zurb.com/sites/docs/media-queries.html
   $('#responsive-menu li a, .top-bar-title a').click(function() {
-    var foundationWidth = Foundation.MediaQuery.current;
 
-    if( foundationWidth == 'small' || 'medium'){
-        $('#responsive-menu').css('display', 'none');
+    if (Foundation.MediaQuery.atLeast('large')) {
+      // True if large
+      $('#responsive-menu').css('display', 'inline');
     }
     else {
-      return;
+      $('#responsive-menu').css('display', 'none');
     }
+
+    //if($(window).width() < 1024) {
+      //$('#responsive-menu').css('display', 'none');
+    //}
+
+
   });
 
 
