366791d00b972a91c48a71e60bfcc31e5ad21558
Longster <longster@outlook.com>
2017-03-10 06:15:23 -0600

    clean up

--- lduong/src/assets/js/app.js
+++ lduong/src/assets/js/app.js
@@ -4,28 +4,28 @@ $(document).ready(function() {
 
 
 	// grab the initial top offset of the navigation
-// 	var stickyNavTop = $('#navbar').offset().top;
+ 	var stickyNavTop = $('#navbar').offset().top;
 
  	// function that decides wether the navigation bar should have "fixed" css position or not.
-// 	var stickyNav = function()  {
-//    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+ 	var stickyNav = function()  {
+    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 
     // if we've scrolled more than the navigation, change its position to fixed to stick to top,
     // otherwise change it back to relative
-//    if (scrollTop > stickyNavTop) {
-//        $('#navbar').addClass('tacky');
-//        $('.tackster').addClass('tacky-hack');
-//    } else {
-//        $('#navbar').removeClass('tacky');
-//        $('.tackster').removeClass('tacky-hack');
-//    }
-//	};
+    if (scrollTop > stickyNavTop) {
+        $('#navbar').addClass('tacky');
+        $('.tackster').addClass('tacky-hack');
+    } else {
+        $('#navbar').removeClass('tacky');
+        $('.tackster').removeClass('tacky-hack');
+    }
+	};
 
-//	stickyNav();
+	stickyNav();
 	// and run it again every time you scroll
-//	$(window).scroll(function() {
-//		stickyNav();
-//	});
+	$(window).scroll(function() {
+		stickyNav();
+	});
 
   // Collapse responsive-menu after clicking link when on responsive mode:
   // http://stackoverflow.com/questions/18516696/how-can-i-close-the-foundation-topbar-menu-when-a-link-is-clicked
